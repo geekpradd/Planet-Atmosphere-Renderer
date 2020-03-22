529380e7de54f9fd76b581a21eb6caec7df774a9
geekpradd <geekpradd@gmail.com>
2020-03-22 19:39:55 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Planet and Atmosphere Renderer Open GL
+
+This project is done under IITB Seasons of Code
+
+All project related and tutorial files will be kept here.
---
+++ planet/demo.cpp
@@ -0,0 +1,43 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void resize(GLFWwindow *w, int width, int height){
+    glViewport(0, 0, width, height);
+}
+
+void processinput(GLFWwindow *w){
+    if (glfwGetKey(w, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+        glfwSetWindowShouldClose(w, true);
+    }
+    if (glfwGetKey(w, GLFW_KEY_I) == GLFW_PRESS){
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+}
+
+int main(){
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* win = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
+
+    glfwMakeContextCurrent(win);
+
+    glewExperimental = GL_TRUE;
+    glewInit();
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(win, resize);
+
+    while(!glfwWindowShouldClose(win)){
+        processinput(win);
+        glfwSwapBuffers(win);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+
+}
