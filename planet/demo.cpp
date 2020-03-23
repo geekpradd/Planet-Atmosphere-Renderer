0dcaee8eda073be462678ecfeac9e081c07a1aff
geekpradd <geekpradd@gmail.com>
2020-03-23 17:24:34 +0530

    Add separate folder for binary executables

--- planet/demo.cpp
+++ planet/demo.cpp
@@ -1,7 +1,7 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-#include "utility.hpp"
+#include "include/utility.hpp"
 
 void resize(GLFWwindow *w, int width, int height){
     glViewport(0, 0, width, height);
