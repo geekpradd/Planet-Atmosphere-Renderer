df00a7e4c38a11f1468b02100df74bd4cb29dd90
geekpradd <geekpradd@gmail.com>
2020-03-31 21:49:24 +0530

    Add camera class and modify lighting_sphere to use it

--- planet/include/camera.hpp
+++ planet/include/camera.hpp
@@ -5,9 +5,76 @@
 #include "glm/gtc/type_ptr.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
-
 class Camera {
 
+public:
+    glm::vec3 cameraPos; // +Z
+    glm::vec3 upperVec; //+Y
+    glm::vec3 rightVec; // +X
+    glm::vec3 lookAt;
+    float yaw = -90.0f; // yaw is measured wrt x axis in xz plane (rotation axis is y)
+    float pitch = 0.0f; // pitch is measured wrt z axis in yz plane (rotation axis is x)
+    float curTime = glfwGetTime();
+    float deltaTime = 0.0f;
+    float fov = 45.0f;
+    float x_glob, y_glob, speed;
 
+    Camera(glm::vec3 initialPosition, glm::vec3 upperDirection, glm::vec3 lookDir, float width=800, float height=600){
+        cameraPos = initialPosition;
+        upperVec = upperDirection;
+        lookAt = lookDir;
 
+        rightVec = glm::normalize(glm::cross(upperVec, cameraPos)); // +X
+        x_glob = width/2; y_glob = height/2;
+
+    }
+    void modify_pitch_yaw(float delta_x, float delta_y){
+        float senstivity = 0.05f;
+        pitch += senstivity*delta_y;
+        yaw += senstivity*delta_x;
+
+        if (pitch > 89.0f)
+            pitch = 89.0f;
+        if (pitch < -89.0f)
+            pitch = -89.0f;
+    }
+    
+    void modify_fov(double y_offset){
+        if (fov > 1.0f && fov <= 45.0f){
+            fov -= y_offset;
+        }
+        else if (fov<=1.0f){
+            fov= 1.0f;
+        }
+        else if (fov > 45.0f){
+            fov = 45.0f;
+        }
+    }
+    
+    void updateTimeCounter(){
+        deltaTime = glfwGetTime() - curTime;
+        curTime = glfwGetTime();
+        speed = 5000.0f*deltaTime; 
+    }
+    void updateLookAt(){
+        lookAt.y = sin(glm::radians(pitch));
+        lookAt.x = cos(glm::radians(pitch))*cos(glm::radians(yaw));
+        lookAt.z = cos(glm::radians(pitch))*sin(glm::radians(yaw)); 
+        rightVec = glm::normalize(glm::cross(lookAt, upperVec));
+    }
+
+    glm::mat4 getViewMatrix(){
+        return glm::lookAt(cameraPos, cameraPos+lookAt, upperVec);
+    }
+    
+    void update_camera_position(int key, int action){
+        if (key == GLFW_KEY_A && action == GLFW_PRESS)
+            cameraPos = (cameraPos - speed*rightVec);
+        else if (key == GLFW_KEY_D && action == GLFW_PRESS)
+            cameraPos = (cameraPos + speed*rightVec);
+        else if (key == GLFW_KEY_W && action == GLFW_PRESS)
+            cameraPos = (cameraPos + speed*lookAt);
+        else if (key == GLFW_KEY_S && action == GLFW_PRESS)
+            cameraPos = (cameraPos - speed*lookAt);
+    }
 };
--- planet/lighting_sphere.cpp
+++ planet/lighting_sphere.cpp
@@ -8,74 +8,19 @@
 #include "glm/gtc/type_ptr.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "include/sphere.hpp"
+#include "include/camera.hpp"
 #define STB_IMAGE_IMPLEMENTATION
 #include "include/stb_image.h"
+bool DEBUG=true;
+glm::vec3 initialCameraPos = glm::vec3(0.0f, 0.0f, 6.0f); // +Z
+glm::vec3 initialUpperVec = glm::vec3(0.0f, 1.0f, 0.0f); //+Y
+glm::vec3 initialLookAt = glm::vec3(0.0f, 0.0f, -1.0f);
+Camera camera(initialCameraPos, initialUpperVec, initialLookAt);
 
-glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 6.0f); // +Z
-glm::vec3 upperVec = glm::vec3(0.0f, 1.0f, 0.0f); //+Y
-glm::vec3 rightVec = glm::normalize(glm::cross(upperVec, cameraPos)); // +X
-glm::vec3 lookAt = glm::vec3(0.0f, 0.0f, -1.0f);
-float yaw = -90.0f; // yaw is measured wrt x axis in xz plane (rotation axis is y)
-float pitch = 0.0f; // pitch is measured wrt z axis in yz plane (rotation axis is x)
-float curTime = glfwGetTime();
-float deltaTime = 0.0f;
-
-void reshape_viewport(GLFWwindow *w, int width, int height){
-    glViewport(0, 0, width, height);
-}
-
-float x_glob = 400;
-float y_glob = 300;
-void cursor_callback(GLFWwindow* window, double x, double y){
-    float delta_x = x - x_glob;
-    float delta_y = y_glob - y;
-    x_glob = x;
-    y_glob = y;
-
-    float senstivity = 0.05f;
-    pitch += senstivity*delta_y;
-    yaw += senstivity*delta_x;
-
-
-    if (pitch > 89.0f)
-        pitch = 89.0f;
-    if (pitch < -89.0f)
-        pitch = -89.0f;
-}
-float fov = 45.0f;
-
-void scroll_callback(GLFWwindow* w, double x, double y){
-    if (fov > 1.0f && fov <= 45.0f){
-        fov -= y;
-    }
-    else if (fov<=1.0f){
-        fov= 1.0f;
-    }
-    else if (fov > 45.0f){
-        fov = 45.0f;
-    }
-}
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-  {
-    //!Close the window if the ESC key was pressed
-    float speed = 5000.0f*deltaTime;
-
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-      glfwSetWindowShouldClose(window, GL_TRUE);
-    else if (key == GLFW_KEY_A && action == GLFW_PRESS)
-      cameraPos = (cameraPos - speed*rightVec);
-    else if (key == GLFW_KEY_D && action == GLFW_PRESS)
-      cameraPos = (cameraPos + speed*rightVec);
-    else if (key == GLFW_KEY_W && action == GLFW_PRESS)
-      cameraPos = (cameraPos + speed*lookAt);
-    else if (key == GLFW_KEY_S && action == GLFW_PRESS)
-      cameraPos = (cameraPos - speed*lookAt);
-
-    if (key != GLFW_KEY_ESCAPE){
-        std::cout << "Current Position is " << cameraPos.x << ", " << cameraPos.y << ", " << cameraPos.z << std::endl;
-    }
-    
-    }
+void reshape_viewport(GLFWwindow *w, int width, int height);
+void cursor_callback(GLFWwindow* window, double x, double y);
+void scroll_callback(GLFWwindow* w, double x, double y);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 int main(){
     glfwInit();
@@ -83,8 +28,7 @@ int main(){
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* w = glfwCreateWindow(800, 600, "Triangle", NULL, NULL);
-
+    GLFWwindow* w = glfwCreateWindow(800, 600, "Sphere", NULL, NULL);
     glfwMakeContextCurrent(w);
     glewExperimental = GL_TRUE;
     glewInit();
@@ -110,18 +54,12 @@ int main(){
     while (!glfwWindowShouldClose(w)){
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
         
-        deltaTime = glfwGetTime() - curTime;
-        curTime = glfwGetTime(); 
-        glm::mat4 projection = glm::perspective(glm::radians(fov), 800.0f/600.0f, 0.1f, 100.0f);
+        camera.updateTimeCounter();
+        glm::mat4 projection = glm::perspective(glm::radians(camera.fov), 800.0f/600.0f, 0.1f, 100.0f);
 
         //for camera we will exclusively modify only the view matrix
-        lookAt.y = sin(glm::radians(pitch));
-        lookAt.x = cos(glm::radians(pitch))*cos(glm::radians(yaw));
-        lookAt.z = cos(glm::radians(pitch))*sin(glm::radians(yaw)); 
-        rightVec = glm::normalize(glm::cross(lookAt, upperVec)); // +X
-
-
-        glm::mat4 view = glm::lookAt(cameraPos, cameraPos+lookAt, upperVec);
+        camera.updateLookAt();
+        glm::mat4 view = camera.getViewMatrix();
         glm::mat4 model = glm::mat4(1.0f);
 
         shdr->use();
@@ -129,7 +67,7 @@ int main(){
         shdr->setMatrix4f("model", model);
         glm::mat4 normalMatrix = glm::transpose(glm::inverse(model));
         shdr->setMatrix4f("normalMatrix", normalMatrix);
-        shdr->setVec3f("viewLoc", cameraPos);
+        shdr->setVec3f("viewLoc", camera.cameraPos);
         shdr->setMatrix4f("view", view);
         shdr->setMatrix4f("projection", projection);
         shdr->setVec3f("lightColor", lightColor);
@@ -153,3 +91,31 @@ int main(){
     glfwTerminate();
     return 0;
 }
+
+void reshape_viewport(GLFWwindow *w, int width, int height){
+    glViewport(0, 0, width, height);
+}
+void cursor_callback(GLFWwindow* window, double x, double y){
+    float delta_x = x - camera.x_glob;
+    float delta_y = camera.y_glob - y;
+    camera.x_glob = x;
+    camera.y_glob = y;
+    camera.modify_pitch_yaw(delta_x, delta_y);
+}
+void scroll_callback(GLFWwindow* w, double x, double y){
+    camera.modify_fov(y);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    else 
+        camera.update_camera_position(key, action);
+    
+    if (key != GLFW_KEY_ESCAPE && DEBUG){
+        std::cout << "Current Position is " << camera.cameraPos.x << ", " << camera.cameraPos.y << ", " << camera.cameraPos.z << std::endl;
+    }
+    
+}
+    
